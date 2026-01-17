3059ed500ea47a76b75b968a4ef3c10787a9df68
Santosh Nandiyawar <nandiyawarsantosh.0719@gmail.com>
2026-01-17 22:01:10 +0000

    Updated:- Frontend-Link

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ const teacherRoutes = require('./routes/teacherRoutes')
 connectDB();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+    origin: [
+        "https://itb-tution.vercel.app", 
+        "http://localhost:5173"          
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/api', userRoutes);
