e3880532b6876285f5a8c5cf413bd261938011e7
Santosh Nandiyawar <nandiyawarsantosh.0719@gmail.com>
2026-01-18 16:15:32 +0000

    Refactored:- Email verification

--- utils/emailService.js
+++ utils/emailService.js
@@ -3,24 +3,27 @@ import nodemailer from 'nodemailer';
 export const sendVerificationEmail = async (userEmail, token) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587, 
+        port: 587,
         secure: false, 
+        service: 'gmail', 
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
-        // connectionTimeout: 10000, 
-        tls: {
-        rejectUnauthorized: false 
-    }
+        connectionTimeout: 10000, // 10 seconds
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
+        dnsServer: '8.8.8.8', 
+        debug: true, 
+        logger: true 
     });
 
-    const url = `https://itb-tution.vercel.app/verify-email/${token}`; 
+    const url = `https://itb-tution.vercel.app/verify-email/${token}`;
 
     await transporter.sendMail({
         from: `"ITB Tuition" <${process.env.EMAIL_USER}>`,
         to: userEmail,
         subject: "Verify Your Account",
-        html: `Click <a href="${url}">here</a> to verify.`,
+        html: `<h3>Welcome to ITB</h3><p>Click <a href="${url}">here</a> to verify.</p>`,
     });
 };
